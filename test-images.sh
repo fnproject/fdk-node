48ee61011267ae28a4ac1a226ce55f246ff7a93c
Denis Makogon <denys.makogon@oracle.com>
2019-04-19 14:26:54 +0300

    Try Anchore that is built-in into CircleCI

--- test-images.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-nodeversion=${1:-"8"}
-
-snyk test --docker fnproject/node:${nodeversion}-dev --file=images/build-stage/${nodeversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
-snyk test --docker fnproject/node:${nodeversion} --file=images/runtime/${nodeversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
