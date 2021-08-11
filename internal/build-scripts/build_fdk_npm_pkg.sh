1e69a9ad4ef8557c0b90778db86c9b36058b0adf
PREETI MISRA <preeti.misra@oracle.com>
2021-08-11 13:16:29 +0530

    Fix latest vulnerabilities with npm audit fix

--- internal/build-scripts/build_fdk_npm_pkg.sh
+++ internal/build-scripts/build_fdk_npm_pkg.sh
@@ -5,10 +5,15 @@ CURRENT_PKG_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ pri
 export CURRENT_PKG_VERSION
 
 (
+  # Node version is being echoed here as it should be consistent with the latest runtime supported by fncli
   echo "node Version"
   node --version
+
+  # npm -v is being echoed here as it should be consistent with version of the node package manager (npm)
+  # which comes along with the latest node runtime supported by fncli
   echo "npm Version"
   npm -v
+
   echo "Perform npm install to ensure package is not broken"
   npm install
 
