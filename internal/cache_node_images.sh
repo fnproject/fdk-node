0b7545ac8e06d2778b31537cd6d9dcbe75d53954
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-10-13 11:08:39 +0530

    FAAS-6982: Upgrade to OL8

--- internal/cache_node_images.sh
+++ internal/cache_node_images.sh
@@ -6,9 +6,6 @@ set -ex
 # Hence, one may encounter rate limiting issue while accessing the node docker images which are not present in artifactory.
 # In order to resolve the rate limiting issue, the below script helps to pull the node images from docker hub and cache them in artifactory.
 
-# Node 14
-docker pull docker-remote.artifactory.oci.oraclecorp.com/node:14.17.6-alpine
-
 # Node 11
 docker pull docker-remote.artifactory.oci.oraclecorp.com/node:11.15.0-alpine
 
