7e14080128fc4c1eb5c8bdbc327a10c6b5d1084d
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-12-13 11:57:01 +0530

    Started using OL8 as base

--- internal/cache_node_images.sh
+++ internal/cache_node_images.sh
@@ -6,11 +6,10 @@ set -ex
 # Hence, one may encounter rate limiting issue while accessing the node docker images which are not present in artifactory.
 # In order to resolve the rate limiting issue, the below script helps to pull the node images from docker hub and cache them in artifactory.
 
-# Node 14
-docker pull docker-remote.artifactory.oci.oraclecorp.com/node:14.17.6-alpine
-
 # Node 11
 docker pull docker-remote.artifactory.oci.oraclecorp.com/node:11.15.0-alpine
 
 # Node 9 for unit test execution
 docker pull docker-remote.artifactory.oci.oraclecorp.com/node:9-alpine
+
+docker pull docker-remote.artifactory.oci.oraclecorp.com/oraclelinux:8-slim
