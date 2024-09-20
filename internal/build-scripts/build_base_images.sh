ad0234661f54823ec82139c0d6051df83f009e40
Alex Shand <alex.shand@oracle.com>
2024-09-20 14:46:12 +0100

    Add publish to github script and remove files only used by Teamcity

--- internal/build-scripts/build_base_images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-(
-  # Build base fdk build and runtime images
-  ./internal/build-scripts/build_base_image.sh 20
-  ./internal/build-scripts/build_base_image.sh 18
-)
--- internal/build-scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-if [ "${LOCAL}" = "true" ] && [ "${RUN_TYPE}" != "release" ]; then
-  # Remove locally built package in case of local run
-  rm -rf fnproject-fdk-${BUILD_VERSION}.tgz
-fi
