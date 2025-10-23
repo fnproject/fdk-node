d049f4437992a0754b1065e09d76e10f5b1dcc8b
Preeti Misra <preeti.misra@oracle.com>
2025-10-23 22:14:21 +0530

    Remove support for node v20 to address security findings CVE-2025-22150

--- internal/release/docker_publish.sh
+++ internal/release/docker_publish.sh
@@ -15,8 +15,6 @@ function cleanup {
 trap cleanup EXIT
 
 {
-$REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/node:20 docker.io/fnproject/node:20;
-$REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/node:20-dev docker.io/fnproject/node:20-dev;
 $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/node:22 docker.io/fnproject/node:22;
 $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/node:22-dev docker.io/fnproject/node:22-dev;
 }
